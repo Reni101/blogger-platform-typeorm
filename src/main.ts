3c427455ed8f184b5c3a0228845d61720dcde029
Maxim D <renissance@tut.by>
2026-07-12 18:28:50 +0300

    fix: vercel.json

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,21 @@
+import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { Logger } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { appSetup } from './setup/app.setup';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
-    const config = app.get(ConfigService);
     appSetup(app); //глобальные настройки приложения
     app.enableCors();
     app.use(cookieParser());
-    app.set('trust proxy', 'loopback');
+    app.set('trust proxy', 1);
 
-    const port = config.getOrThrow<string>('PORT');
+    const port = Number(process.env.PORT ?? 3000);
 
-    await app.listen(+port);
+    await app.listen(port);
     Logger.log(`🚀 Swagger on: http://localhost:${port}/swagger`);
 }
 bootstrap();
