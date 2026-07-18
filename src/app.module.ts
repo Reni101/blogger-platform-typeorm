ae7d9e3c0cbdc0a93d3d74a91bc22004da70bd0a
Maxim D <renaissance101010@gmail.com>
2026-07-18 12:27:16 +0300

    Revert "fix: size"
    This reverts commit 80cd7b581853e669dfb1a0ad0aae55273acf6243.

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,11 +24,18 @@ import { MongooseModule } from '@nestjs/mongoose';
         TypeOrmModule.forRootAsync({
             inject: [ConfigService],
             useFactory: (config: ConfigService) => {
+                const isProduction =
+                    config.get<string>('NODE_ENV') === 'production';
+
                 return {
                     type: 'postgres' as const,
                     url: config.getOrThrow<string>('PG_URL'),
                     autoLoadEntities: true,
                     synchronize: false,
+                    ssl: isProduction
+                        ? { rejectUnauthorized: false }
+                        : false,
+                    // logging: true,
                 };
             },
         }),
