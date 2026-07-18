6c0d2653a9fd0e00922114879b7cb89707188293
Maxim D <renaissance101010@gmail.com>
2026-07-18 12:02:20 +0300

    feat: fix vercel build

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
+import {
+    ExpressAdapter,
+    NestExpressApplication,
+} from '@nestjs/platform-express';
 import { Logger } from '@nestjs/common';
 import { appSetup } from './setup/app.setup';
 import cookieParser from 'cookie-parser';
+import express from 'express';
+import serverless from 'serverless-http';
+
+// eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
+let cachedServer: serverless.Handler | null = null;
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -12,10 +20,40 @@ async function bootstrap() {
     app.enableCors();
     app.use(cookieParser());
     app.set('trust proxy', 1);
-    // git commit
     const port = Number(process.env.PORT ?? 3000);
 
     await app.listen(port);
     Logger.log(`🚀 Swagger on: http://localhost:${port}/swagger`);
 }
-bootstrap();
+
+async function createServer(): Promise<serverless.Handler> {
+    const expressApp = express();
+    const adapter = new ExpressAdapter(expressApp);
+    const app = await NestFactory.create<NestExpressApplication>(
+        AppModule,
+        adapter,
+        {
+            logger: ['error', 'warn', 'log'],
+        },
+    );
+    appSetup(app);
+    app.enableCors();
+    app.use(cookieParser());
+    app.set('trust proxy', 1);
+    await app.init();
+    return serverless(expressApp);
+}
+
+export default async function handler(
+    req: express.Request,
+    res: express.Response,
+): Promise<void> {
+    if (!cachedServer) {
+        cachedServer = await createServer();
+    }
+    await cachedServer(req, res);
+}
+
+if (!process.env.VERCEL) {
+    bootstrap();
+}
